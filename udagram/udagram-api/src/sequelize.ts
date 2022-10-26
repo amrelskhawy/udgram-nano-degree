bf997550646d05236c440f306e213450792c1d7d
amrelskhawy <amrelskhawy@gmail.com>
2022-10-26 14:01:57 -0400

    Rebuild Project

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: config.username,
-  password: (config.password as string),
-  database: config.database,
-  host: config.host,
-
+  username: "postgres",
+  password: "password",
+  database: "postgres",
+  host: "database-1.cgdvndqxx1xc.us-east-1.rds.amazonaws.com",
+  
   dialect: "postgres",
   storage: ":memory:",
 });
